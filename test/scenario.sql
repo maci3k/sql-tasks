88bffe0acbc23d02acadaea9faa37163b99b8510
aniaw <ania_w@outlook.com>
2016-05-19 15:36:05 +0200

    update scaffolding

--- test/scenario.sql
+++ test/scenario.sql
@@ -1,49 +1,24 @@
---statement="./schema.sql"
---statement="./data.sql"
+--statement="./statements/schema.sql"
+--statement="./statements/insert_data.sql"
 
 --statement="../solution/search1.sql"
---expect rows containing 'friend' and 'family' in field temperament
-id,name
-11,Golden Retriever
-14,Newfoundland
+--expect="expectations/expected_dogs.csv" rows containing 'friend' and 'family' in field temperament
 
 --statement="../solution/search2.sql"
---expect rows that contains 'hunting' or 'gundog' or 'retrieve' in `family`, `original_function` or `temperament`
-id,name
-1,Akita
-2,American Cocker Spaniel
-11,Golden Retriever
-12,Irish Setter
-13,Labrador Retriever
-17,Yorkshire Terrier
+--expect="expectations/expected_dogs2.csv" rows that contains 'hunting' or 'gundog' or 'retrieve' in `family`, `original_function` or `temperament`
 
 --statement="../solution/search3.sql"
---expect rows that contains 'herding' or 'sheepdog' or 'livestock' but not contain 'mastiff'; in `family` or `original_function`
-id,name
-8,Collie
-9,Border Collie
-18,Shetland Sheepdog
-20,German Shepherd Dog
+--expect="expectations/expected_dogs3.csv" rows that contains 'herding' or 'sheepdog' or 'livestock' but not contain 'mastiff'; in `family` or `original_function`
 
---statement="./data2.sql"
+--statement="./statements/insert_data2.sql"
 --statement="../solution/search1.sql"
---expect rows containing 'friend' and 'family' in field temperament
-id,name
-21,Akita
-23,Beagle
-29,Newfoundland
+--expect="expectations/expected_dogs4.csv" rows containing 'friend' and 'family' in field temperament
 
 --statement="../solution/search2.sql"
---expect rows that contains 'hunting' or 'gundog' or 'retrieve' in `family`, `original_function` or `temperament`
-id,name
-21,Akita
-22,American Cocker Spaniel
+--expect="expectations/expected_dogs5.csv" rows that contains 'hunting' or 'gundog' or 'retrieve' in `family`, `original_function` or `temperament`
 
 --statement="../solution/search3.sql"
---expect rows that contains 'herding' or 'sheepdog' or 'livestock' but not contain 'mastiff'; in `family` or `original_function`
-id,name
-28,Collie
-29,Newfoundland
+--expect="expectations/expected_dogs6.csv" rows that contains 'herding' or 'sheepdog' or 'livestock' but not contain 'mastiff'; in `family` or `original_function`
 
 --required expression="to_tsvector" statement="../solution/search1.sql"
 --required expression="to_tsquery" statement="../solution/search1.sql"
