6de056883b6310e73b689f3083b552b8728de500
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-02 09:01:18 +0200

    Task completed

--- solution/query.sql
+++ solution/query.sql
@@ -1,1 +1,7 @@
 -- Write your solution here
+ALTER TABLE users ADD COLUMN email TEXT UNIQUE;
+
+UPDATE users u SET email = (SELECT 'test' || p.id || '@test.com' FROM users p WHERE u.id=p.id);
+
+ALTER TABLE users ALTER COLUMN email SET NOT NULL;
+ALTER TABLE users ALTER COLUMN email DROP DEFAULT;
