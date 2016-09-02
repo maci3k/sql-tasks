83611358405b4cd2767cbd07620028e168a59cba
Maciej Klimczyk <jackson93.07@gmail.com>
2016-09-02 09:03:03 +0200

    Removed useless line

--- solution/query.sql
+++ solution/query.sql
@@ -4,4 +4,3 @@ ALTER TABLE users ADD COLUMN email TEXT UNIQUE;
 UPDATE users u SET email = (SELECT 'test' || p.id || '@test.com' FROM users p WHERE u.id=p.id);
 
 ALTER TABLE users ALTER COLUMN email SET NOT NULL;
-ALTER TABLE users ALTER COLUMN email DROP DEFAULT;
